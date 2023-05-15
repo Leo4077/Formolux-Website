bfbd6e8b8f6c993205c842bc8b63310d6aa0ec0d
Alicezhao0111 <goodchild40723@gmail.com>
2023-05-15 16:14:19 +0800

    05/15 travel style

--- 20230515 Travel Style/test01.js
+++ 20230515 Travel Style/test01.js
@@ -12,3 +12,20 @@ new Swiper("#swiper-2", {
         prevEl: "#stylenav-left"
     },
 });
+
+// page scroll頁面滾動
+var slider = document.getElementById('slider-2');
+var isScrolling = false;
+
+window.addEventListener('scroll', function() {
+  var sliderPosition = slider.getBoundingClientRect().top;
+  var windowHeight = window.innerHeight;
+
+  if (!isScrolling && sliderPosition - 500 <= 0) {
+    isScrolling = true;
+    slider.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' });
+    setTimeout(function() {
+      isScrolling = false;
+    }, 1000);
+  }
+});
