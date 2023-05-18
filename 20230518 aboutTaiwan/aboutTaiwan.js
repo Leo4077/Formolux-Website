74ea77fe3eb98ec55e4556c7592cb51cf48275c2
Alicezhao0111 <goodchild40723@gmail.com>
2023-05-18 20:18:07 +0800

    2023/05/18
    about Taiwan

---
+++ 20230518 aboutTaiwan/aboutTaiwan.js
@@ -0,0 +1,11 @@
+
+// 資訊區smoove
+    $('.smoove').smoove({
+        offset : '25%',
+        moveX  : '-15px',
+    });
+
+    $('.smoove_z').smoove({
+        offset : '25%',
+        moveX  : '15px',    
+    });
