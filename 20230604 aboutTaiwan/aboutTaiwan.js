065ebb5ccb63adc459c1a4b01ee0d105c6083cc5
Alicezhao0111 <goodchild40723@gmail.com>
2023-06-15 19:10:18 +0800

    0615

--- 20230604 aboutTaiwan/aboutTaiwan.js
+++ 20230604 aboutTaiwan/aboutTaiwan.js
@@ -3,7 +3,7 @@ const parallaxes = document.querySelectorAll('.taiwanpic')
 
 parallaxes.forEach(parallax => {
     new Ukiyo(parallax, {
-        scale: 1.05, // 1~2 is recommended
+        scale: 1.1, // 1~2 is recommended
         speed: 1.3, // 1~2 is recommended
         willChange: true, // This may not be valid in all cases
         // wrapperClass: "ukiyo-wrapper",
--- 20230604 travelstyle/test01.js
+++ 20230604 travelstyle/test01.js
@@ -1,3 +1,4 @@
+/*桌機板swiper*/
 new Swiper("#swiper-2", {
   slidesPerView: 3.6,
   centeredSlides: false,
@@ -13,15 +14,18 @@ new Swiper("#swiper-2", {
   },
 });
 
+/*平板swiper*/
+
 // if (window.matchMedia("(min-width: 577px) and (max-width: 1024px)").matches) {
 //   new Swiper("#swiper-2", {
-//     slidesPerView: 4.5,
+
+//     slidesPerView: 3.6,
+//     spaceBetween: 35,
 //     centeredSlides: false,
-//     spaceBetween: 20,
 //     lazy: true,
 //     loop: true,
 //     keyboard: {
-//       enabled: true,
+//       enabled: false,
 //     },
 //     navigation: {
 //       nextEl: "#stylenav-right",
@@ -30,6 +34,7 @@ new Swiper("#swiper-2", {
 //   });
 // }
 
+/*桌機板:點選樣式改變*/
 
 var cardDetails = {
   solo: {
@@ -63,12 +68,11 @@ var cardDetails = {
 };
 
 document.addEventListener("DOMContentLoaded", function () {
-  // 獲取要改變背景圖片的元素
   var travelBG = document.querySelector("#slider-2");
-  
+
   // 為每一個 "a" 元素添加點擊事件監聽器
-  document.querySelectorAll("#swiper-2 a").forEach(function(el) {
-    el.addEventListener("click", function(event) {
+  document.querySelectorAll("#swiper-2 a").forEach(function (el) {
+    el.addEventListener("click", function (event) {
       event.preventDefault();
 
       // 獲取對應的對象
@@ -78,17 +82,98 @@ document.addEventListener("DOMContentLoaded", function () {
         return;
       }
 
-      // 更新背景图片
+      // 更新背景圖片
       travelBG.style.backgroundImage = details.bg;
 
-      // 更新其他元素的内容
+      // 更新其他元素的內容
       document.getElementById("slider-text-1").textContent = details.text1;
       document.getElementById("slider-text-2").innerHTML = details.text2;
       document.getElementById("slider-text-3").textContent = details.text3;
       document.getElementById("slider-text-4").textContent = details.text4;
     });
   });
+
+  /*平板:點選樣式改變*/
+  // 添加平板裝置的事件監聽器
+  function tabletDeviceHandler(event) {
+    event.preventDefault();
+
+    var details;
+    var windowWidth = window.innerWidth;
+
+    // 根據裝置寬度選擇對應的卡片細節
+    if (windowWidth >= 577 && windowWidth <= 1024) {
+      // 平板裝置的卡片細節
+      details = {
+        solo: {
+          text1: "1 person",
+          text2: "SOLO TRAVEL",
+          text3: "# spiritual # cultural # fineart",
+          text4: "Experience a valuable travel that allows one to explore oneself, and grow.",
+          bg: "url('../images/ipad_solo_BG.png')"
+        },
+        family: {
+          text1: "3-6 people",
+          text2: "FAMILY TRIP",
+          text3: "# nature  # animals  # spring",
+          text4: "Sharing joy to strengthen familial bonds, interacting to deepen affections.",
+          bg: "url('../images/ipad_family_BG.png')"
+        },
+        besties: {
+          text1: "2-4 people",
+          text2: "BESTIES TRIP",
+          text3: "# spa # relaxing # photoshoot",
+          text4: "Enjoying a luxurious trip with my best friend like a privileged lady, relaxing both mind and body.",
+          bg: "url('../images/ipad_besties_BG.png')"
+        },
+        honeymoonCard: {
+          text1: "2 people",
+          text2: "HONEY MOON",
+          text3: "# romantic # sweet # night view",
+          text4: "Experience a luxurious and laid-back honeymoon trip, unwind and create unforgettable memories with your loved one.",
+          bg: "url('../images/ipad_travelsty_BG.png')"
+        },
+      };
+
+      // 更新背景圖片
+      travelBG.style.backgroundImage = details[this.id].bg;
+
+      // 更新其他元素的內容
+      document.getElementById("slider-text-1").textContent = details[this.id].text1;
+      document.getElementById("slider-text-2").innerHTML = details[this.id].text2;
+      document.getElementById("slider-text-3").textContent = details[this.id].text3;
+      document.getElementById("slider-text-4").textContent = details[this.id].text4;
+    }
+  }
+
+  // 綁定平板裝置的點擊事件
+  document.querySelectorAll("#swiper-2 a").forEach(function (el) {
+    el.addEventListener("click", tabletDeviceHandler);
+  });
+
+  // 添加視窗大小變化時的事件監聽器，以便在平板裝置的寬度範圍內更新內容
+  window.addEventListener("resize", function () {
+    var windowWidth = window.innerWidth;
+    if (windowWidth >= 577 && windowWidth <= 1024) {
+      // 觸發平板裝置的點擊事件
+      var activeLink = document.querySelector("#swiper-2 a.active");
+      if (activeLink) {
+        activeLink.dispatchEvent(new Event("click"));
+      }
+    }
+  });
+
+  // 在頁面加載時觸發平板裝置的點擊事件
+  var initialWindowWidth = window.innerWidth;
+  if (initialWindowWidth >= 577 && initialWindowWidth <= 1024) {
+    var activeLink = document.querySelector("#swiper-2 a.active");
+    if (activeLink) {
+      activeLink.dispatchEvent(new Event("click"));
+    }
+  }
 });
+
+
 
 
 
