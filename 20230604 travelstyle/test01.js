0e279d3174fad41899ddcf5f746222741bab2f51
Alicezhao0111 <goodchild40723@gmail.com>
2023-06-16 17:46:25 +0800

    0616

--- 20230604 travelstyle/test01.js
+++ 20230604 travelstyle/test01.js
@@ -175,286 +175,47 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-
-
-// document.addEventListener('DOMContentLoaded', function () {
-//   const carouselContainer = document.getElementById('swiper-2');
-//   const carouselItems = carouselContainer.getElementsByClassName('swiper-slide');
-//   let count = 0;
-//   let transitionTimeout;
-
-//   // 綁定圖片點擊事件
-//   for (let i = 0; i < carouselItems.length; i++) {
-//     const slide = carouselItems[i];
-//     slide.onclick = function (e) {
-//       e.preventDefault();
-//       const slideIndex = parseInt(slide.getAttribute('index'));
-//       const slideWidth = slide.getBoundingClientRect().width;
-//       const newLeft = slideIndex * slideWidth * -1;
-//       carouselContainer.style.transition = 'left 2s ease 0s';
-//       carouselContainer.style.left = newLeft + 'px';
-
-//       // 停止動畫
-//       clearTimeout(transitionTimeout);
-//       transitionTimeout = setTimeout(() => {
-//         carouselContainer.style.transition = 'none';
-//       }, 2000);
-//     };
-//   }
-// });
-
-
 //滾動至不同section
-$(function() {
-  // 檢查頁面上是否存在 id 為 'pageStyle' 的元素，有的話就執行這段JS
-  if ($("#pageStyle").length > 0) {
-    var scrolling = false; // 追蹤目前是否正在滾動的變數
-    var currentPos = 0; // 追蹤目前所在區塊位置的變數
-    var scrollToSection = $("section"); // 所有 "section" 標籤的集合
 
-    function scrollUp() { // 定義向上滾動的函數
-      // 若目前沒在滾動且不在第一個區塊
-      if (!scrolling && currentPos > 0) { 
-        scrolling = true; // 設定為正在滾動
-        currentPos--; // 往上一個區塊
-        var scrollToElement = scrollToSection[currentPos]; // 取得目前區塊元素
 
-        // 滾動到目前的區塊
-        $("html, body").animate(
-          { scrollTop: $(scrollToElement).offset().top },
-          100,
-          function() { // 滾動完成後的回調函數
-            scrolling = false; // 設定為滾動完畢
-          }
-        );
-      }
-    }
+let isFirstScroll = true;
+let hasVisitedSlider = false;
+let lastScrollY = 0;
 
-    function scrollDown() {
-      if (!scrolling && currentPos < scrollToSection.length - 1) {
-        scrolling = true;
-        currentPos++;
-        var scrollToElement = scrollToSection[currentPos];
-        $("html, body").animate({
-            scrollTop: $(scrollToElement).offset().top
-          }, 100,
-          function() {
-            scrolling = false;
-          });
-      }
-    }
+window.addEventListener('wheel', function(event) {
+  const scrollY = window.pageYOffset;
+  const slider = document.querySelector('#slider-2');
+  const pageStyle = document.querySelector('#pageStyle');
 
-    //初始設定，判斷目前位置
+  // 檢查我們是否在 #pageStyle 區域內
+  const isInPageStyle = scrollY >= pageStyle.offsetTop && scrollY < slider.offsetTop;
 
-    for (var i = 0; i < scrollToSection.length; i++) {
-      var elm = scrollToSection[i];
+  // 檢查滾動方向
+  const isScrollingUp = event.deltaY < 0;
 
-      if ($(document).scrollTop() >= $(elm).offset().top) {
-        currentPos = i;
-      }
-    }
-
-    $(document).on("DOMMouseScroll mousewheel wheel", function(e) {
-      if (e.originalEvent.detail > 0 || e.originalEvent.wheelDelta < 0) {
-        scrollDown(); // 如果滾輪向下，則呼叫 scrollDown 函數
-      } else {
-        scrollUp();
-      }
-      if (currentPos == scrollToSection.length - 1) {
-        console.log("last one");
-      } else {
-        return false;
-      }
+  if ((isFirstScroll && !isScrollingUp) || (isInPageStyle && !isScrollingUp)) {
+    // 不管滾動方向如何，只要滾動滾輪，且滾動方向為向下時，就會跳轉到 #slider-2
+    isFirstScroll = false;
+    hasVisitedSlider = true;
+    window.scrollTo({
+      top: slider.offsetTop,
+      behavior: 'smooth'
+    });
+  } else if (hasVisitedSlider && isScrollingUp && scrollY <= pageStyle.offsetHeight - 5) {
+    // 當用戶向上滾動並且到達 "#pageStyle" 區域的尾端10px處時，跳轉到頁面最頂端
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
     });
   }
+
+  // 如果用戶已經回到頁面頂部，則重設變數以重新觸發滾動效果
+  if (scrollY === 0) {
+    isFirstScroll = true;
+    hasVisitedSlider = false;
+  }
+
+  // 儲存這次的滾動位置以供下次判斷滾動方向
+  lastScrollY = scrollY;
 });
 
-
-// document.addEventListener("DOMContentLoaded", function() {
-//   // 滚动到指定元素的最上方
-//   function scrollToElement() {
-//     var element = document.getElementById("bottom");
-//     var offset = 10; // 滚动10px
-
-//     // 计算目标元素的相对页面顶部的位置
-//     var position = element.offsetTop;
-
-//     // 执行滚动动画
-//     window.scrollTo({
-//       top: position - offset,
-//       behavior: "auto"
-//     });
-//   }
-
-//   // 监听页面滚动事件
-//   function handleScroll() {
-//     var element = document.getElementById("bottom");
-//     var footer = document.getElementById("footerBar");
-//     var elementTop = element.getBoundingClientRect().top;
-//     var footerTop = footer.getBoundingClientRect().top;
-
-//     // 当元素进入视窗时，触发滚动到元素的最上方
-//     if (elementTop <= window.innerHeight && footerTop > window.innerHeight) {
-//       scrollToElement();
-
-//       // 移除滚动事件监听器
-//       window.removeEventListener("scroll", handleScroll);
-//     }
-//   }
-
-//   // 添加滚动事件监听器
-//   window.addEventListener("scroll", handleScroll);
-// });
-
-// /**********************************/ 
-
-// document.addEventListener("DOMContentLoaded", function() {
-//   // 滚动到指定元素的最上方
-//   function scrollToElement(elementId) {
-//     var element = document.getElementById(elementId);
-//     var offset = 10; // 滚动10px
-
-//     // 计算目标元素的相对页面顶部的位置
-//     var position = element.offsetTop;
-
-//     // 执行滚动动画
-//     window.scrollTo({
-//       top: position - offset,
-//       behavior: "auto"
-//     });
-//   }
-
-//   // 监听页面滚动事件
-//   function handleScroll() {
-//     var bottomElement = document.getElementById("bottom");
-//     var pageStyleElement = document.getElementById("pageStyle");
-//     var footer = document.getElementById("footerBar");
-//     var bottomElementTop = bottomElement.getBoundingClientRect().top;
-//     var pageStyleElementTop = pageStyleElement.getBoundingClientRect().top;
-//     var footerTop = footer.getBoundingClientRect().top;
-
-//     // 当元素进入视窗时，触发滚动到元素的最上方
-//     if (bottomElementTop <= window.innerHeight && footerTop > window.innerHeight) {
-//       scrollToElement('bottom');
-//     }
-
-//     if (pageStyleElementTop >= 0) {
-//       scrollToElement('pageStyle');
-//     }
-//   }
-
-//   // 添加滚动事件监听器
-//   window.addEventListener("scroll", handleScroll);
-// });
-
-
-
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   // 獲取要點擊的元素
-//   var soloActive = document.getElementById("solo");
-//   var familyActive = document.getElementById("family");
-//   var bestiesActive = document.getElementById("besties");
-//   var honeyActive = document.getElementById("honeymoonCard"); 
-
-//   // 獲取要改變背景圖片的元素
-//   var travelBG = document.querySelector("#slider-2");
-//   // 獲取要改變LOGO顏色的元素
-//   // var menuLogo = document.querySelector(".menu_logo");
-
-//   // 為 solo 添加點擊事件監聽器
-//   soloActive.addEventListener("click", function () {
-//     event.preventDefault(); //避免頁面跳到上面
-//     travelBG.style.backgroundImage = "url('../images/soloBG.png')";
-
-//   });
-
-//   // 為 family 添加點擊事件監聽器
-//   familyActive.addEventListener("click", function () {
-//     event.preventDefault();
-//     travelBG.style.backgroundImage = "url('../images/familyBG.png') ";
-//       // menuLogo.style.color = "#FFFFFF";
-
-//   });
-
-//   // 為 besties 添加點擊事件監聽器
-//   bestiesActive.addEventListener("click", function () {
-//     event.preventDefault();
-//     travelBG.style.backgroundImage = "url('../images/besties_BG.png')"
-//     // menuLogo.style.color = "#FFFFFF";
-
-//   });
-
-//   // 為 party 添加點擊事件監聽器
-//   contactUs.addEventListener("click", function () {
-//     event.preventDefault();
-//     travelBG.style.backgroundImage = "url('')";
-
-//   });
-// });
-
-
-// 點擊圖像時整個容器向左移動
-
-// var images = document.getElementsByClassName('swiper-slide');
-
-// for (var i = 0; i < images.length; i++) {
-//   images[i].addEventListener('click', function() {
-//     var clickedImage = this;
-//     var parent = clickedImage.parentNode;
-
-//     parent.removeChild(clickedImage); // 从父容器中移除被点击的图像
-//     parent.appendChild(clickedImage); // 将被点击的图像添加到末尾
-
-//     // 更新图像的位置
-//     var children = parent.children;
-//     for (var j = 0; j < children.length; j++) {
-//       var leftPos = (j * 40) + 'px';
-//       children[j].style.left = leftPos;
-//     }
-//   });
-// }
-
-// document.addEventListener('DOMContentLoaded', function() {
-//   var images = document.getElementsByClassName('image');
-
-//   for (var i = 0; i < images.length; i++) {
-//     images[i].addEventListener('click', function() {
-//       var clickedImage = this;
-//       var parent = clickedImage.parentNode;
-//       var containerWidth = parent.offsetWidth;
-//       var imageWidth = clickedImage.offsetWidth;
-//       var containerLeft = parent.offsetLeft;
-//       var clickedImageLeft = clickedImage.offsetLeft;
-//       var offset = containerLeft - clickedImageLeft - imageWidth;
-
-//       clickedImage.style.left = offset + 'px';
-
-//       setTimeout(function() {
-//         parent.removeChild(clickedImage);
-//         parent.appendChild(clickedImage);
-//         var images = parent.getElementsByClassName('image');
-//         for (var j = 0; j < images.length; j++) {
-//           images[j].style.left = '0';
-//         }
-//       }, 500);
-//     });
-//   }
-// });
-
-// const images = document.querySelectorAll('.swiper-slide img');
-// const container = document.querySelector('.swiper-slide');
-
-// container.addEventListener('click', handleClick);
-
-// function handleClick(event) {
-//   if (event.target.tagName === 'IMG') {
-//     const clickedImage = event.target;
-//     const index = Array.from(images).indexOf(clickedImage);
-//     const shiftedImages = Array.from(images).slice(index).concat(Array.from(images).slice(0, index));
-
-//     shiftedImages.forEach((image, i) => {
-//       image.style.transform = `translateX(-${(shiftedImages.length - i - 1) * 280}px)`;
-//     });
-//   }
-// }
